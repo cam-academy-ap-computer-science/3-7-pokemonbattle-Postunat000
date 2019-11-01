f1190b9ac457e6dbad998024ed1ca20fa04f8f5b
postunat000 <postunat000@CAM-LAB-10.bgsd.k12.wa.us>
2019-11-01 07:49:01 -0700

    thisIsAMess.java

--- src/PokemonBattle.java
+++ src/PokemonBattle.java
@@ -1,9 +1,33 @@
-
+import java.util.*;
 public class PokemonBattle {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String pokemonInput = tableStats();
+		
+		battleStart();
+		//damage();
+		tableStats();
 
 	}
-
+	public static String battleStart() {
+		Scanner pokemon = new Scanner(System.in);
+		
+		System.out.println("Another trainer is issuing a challenge!");
+		System.out.println("Zebstrika appeared.");
+		
+		System.out.print("Which pokemon do you choose?" + pokemon);
+		System.out.println();
+		System.out.print("You chose " + pokemon + "!");
+		System.out.println();
+		
+		System.out.print("It's a pokemon battle between " + pokemon + " and Zebstrika! Go!");
+		return pokemon;
+	}
+	
+	//public static void damage() {
+		
+	//}
+	public static String tableStats(String pokemonInput) {
+		System.out.println("Name	" + pokemonInput);
+	}
 }
